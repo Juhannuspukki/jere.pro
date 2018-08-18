543986f8ea84a443491bc35475b238e3f2047ab4
Juhannuspukki <jere.juhani@gmail.com>
2018-08-18 03:13:08 +0300

    Preload fonts

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,8 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="stylesheet" href="/_next/static/style.css" />
+          <link rel="preload" href="/static/fonts/DP-Regular.otf" as="font" crossorigin="anonymous"/>
+          <link rel="preload" href="/static/fonts/DP-Bold.otf" as="font" crossorigin="anonymous"/>
         </Head>
         <body>
         <Main />
