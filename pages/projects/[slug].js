b2bf803504bbf3d5bbf23bcf16c7b951f969bcce
Juhannuspukki <jere@jere.pro>
2020-05-07 22:57:40 +0700

    Fix project page errors

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -4,32 +4,36 @@ import NavBar from '../../components/NavBar'
 import Footer from '../../components/Footer'
 import ProjectContent from "../../content/ProjectContent";
 
-const Project = (props) => {
-  const {name, external, github, link, description, techStack} = props;
-  
+const Project = ({ slug }) => {
+  const findProject = projectList => projectList.find(project => project.link === slug);
+  const document = ProjectContent.projects.find(stackLevel => findProject(stackLevel.projectList));
+
+  const { name, external, github, link, description, techStack } = findProject(document.projectList);
+
+
   return (
     <div>
       <Head>
         <title>jere.pro - Projects - {name}</title>
         <meta property="og:title" content={"jere.pro - Projects - " + name} />
-        <meta property="description" content={ description.replace(/^(.{100}[^\s]*).*/s, "$1") + "..."} />
-        <meta property="og:description" content={ description.replace(/^(.{100}[^\s]*).*/s, "$1") + "..."} />
-        <meta property="og:image" content={ require(`../../public/img/projects/${link}/desktop.jpg`) } />
+        <meta property="description" content={description.replace(/^(.{100}[^\s]*).*/s, "$1") + "..."} />
+        <meta property="og:description" content={description.replace(/^(.{100}[^\s]*).*/s, "$1") + "..."} />
+        <meta property="og:image" content={require(`../../public/img/projects/${link}/desktop.jpg`)} />
         <meta name="robots" content="noindex" />
       </Head>
-      <NavBar url={"/projects"}/>
+      <NavBar url={"/projects"} />
       <div className={"post"}>
         <div className={"container animated"}>
           <video loop={true}
-                 autoPlay={true}
-                 muted={true}
-                 playsInline={true}
-                 controls={false}
-                 className={"hero-desktop"}
-                 aria-hidden={"true"}
-                 poster={require(`../../public/img/projects/${link}/desktop.jpg`)}
+            autoPlay={true}
+            muted={true}
+            playsInline={true}
+            controls={false}
+            className={"hero-desktop"}
+            aria-hidden={"true"}
+            poster={require(`../../public/img/projects/${link}/desktop.jpg`)}
           >
-            <source src={`/img/projects/${link}/desktop.mp4`} type="video/mp4"/>
+            <source src={`/img/projects/${link}/desktop.mp4`} type="video/mp4" />
           </video>
           <h1>{name}</h1>
           <div className="techStack container">
@@ -67,7 +71,7 @@ const Project = (props) => {
           </div>
         </div>
       </div>
-      <Footer url={"/projects"}/>
+      <Footer url={"/projects"} />
     </div>
   )
 }
@@ -75,9 +79,7 @@ const Project = (props) => {
 
 Project.getInitialProps = async ({ query }) => {
   const { slug } = query
-  const findProject = projectList => projectList.find(project => project.link === slug);
-  const document = ProjectContent.projects.find(stackLevel => findProject(stackLevel.projectList));
-  return findProject(document.projectList);
+  return { slug }
 }
 
 export default Project
