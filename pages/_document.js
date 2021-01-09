51c1e3332205e32d4b3c845d452b92facb618551
Juhannuspukki <jere@jere.pro>
2021-01-09 14:56:18 +0200

    Install simpleanalytics

--- pages/_document.js
+++ pages/_document.js
@@ -90,6 +90,17 @@ export default class MyDocument extends Document {
         <body className={Date.now() % 2 === 0 ? "white" : "black"}>
           <Main />
           <NextScript />
+          <script
+            async
+            defer
+            src="https://scripts.simpleanalyticscdn.com/latest.js"
+          ></script>
+          <noscript>
+            <img
+              src="https://queue.simpleanalyticscdn.com/noscript.gif"
+              alt=""
+            />
+          </noscript>
         </body>
       </Html>
     );
