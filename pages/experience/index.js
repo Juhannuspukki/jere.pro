e20242cf1ab2437aa190fecbbc6272ec251be055
Juhannuspukki <jere@jere.pro>
2022-01-22 21:17:23 +0200

    Re-create experience tree

--- pages/experience/index.js
+++ pages/experience/index.js
@@ -2,17 +2,30 @@ import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
 import ExperienceGraph from "../../components/ExperienceGraph";
 import Head from "next/head";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ParentSize } from "@visx/responsive";
 import ExperienceData from "../../content/experience";
 
 const Index = () => {
+  // crap to prevent SSR
+  const [showChild, setShowChild] = useState(false);
+
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if (!showChild) {
+    return null;
+  }
+  // end of crap to prevent SSR
+
   const margin = {
     top: 10,
     left: 10,
     right: 10,
     bottom: 0,
   };
+
   return (
     <div>
       <Head>
@@ -35,7 +48,7 @@ const Index = () => {
             <ExperienceGraph
               data={ExperienceData}
               width={parent.width}
-              height={parent.width > 576 ? 1300 : 2900}
+              height={parent.width > 576 ? 1800 : 3600}
               parentTop={parent.top}
               parentLeft={parent.left}
               parentRef={parent.ref}
