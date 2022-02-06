fc2fbf0617c0130e634c1a26953f20ffbb86cf22
Juhannuspukki <tepzis-Buhteg-xiwbe7>
2022-02-07 00:51:41 +0200

    Show only skills that are used in projects && add sorting function to CategorySegment

--- components/projects/CategorySegment.tsx
+++ components/projects/CategorySegment.tsx
@@ -19,9 +19,29 @@ const CategorySegment: React.FC<CategorySegmentProps> = (props) => {
       : setActiveTechs([...activeTechs, tech]);
   };
 
+  const sortedProjectList = projects.sort((a, b) => {
+    let occurencesInA = 0;
+    let occurencesInB = 0;
+    activeTechs.forEach((element) =>
+      a.techStack.includes(element) ? occurencesInA++ : {}
+    );
+    activeTechs.forEach((element) =>
+      b.techStack.includes(element) ? occurencesInB++ : {}
+    );
+    return occurencesInB - occurencesInA;
+  });
+
   if (typeof skills === "undefined") {
     return null;
   }
+
+  // only show skills that are actually used in projects
+  const usedSkills =
+    name === "UI/UX Design"
+      ? []
+      : skills.filter((skill) =>
+          projects.some((project) => project.techStack.includes(skill.icon))
+        );
 
   return (
     <div className={"category"} key={name}>
@@ -30,33 +50,34 @@ const CategorySegment: React.FC<CategorySegmentProps> = (props) => {
           <h2 className={"rotatedTitle"}>{name}</h2>
         </div>
         {name !== "UI/UX Design" && (
-          <p>
-            <strong>Filters</strong>
-          </p>
+          <>
+            <p>
+              <strong>Filters</strong>
+            </p>
+            {usedSkills.map((skill) => {
+              const Icon = require(`../../svg/graphsymbols/${slugify(
+                skill.icon
+              )}.svg`).default;
+              return (
+                <button
+                  key={skill.icon}
+                  className={
+                    activeTechs.length === 0 || activeTechs.includes(skill.icon)
+                      ? "skillIconContainer highLightOnHover"
+                      : "passive skillIconContainer highLightOnHover"
+                  }
+                  onClick={() => setActiveTech(skill.icon)}
+                >
+                  <Icon />
+                  <p className={"skillTitle chameleon"}>{skill.icon}</p>
+                </button>
+              );
+            })}
+          </>
         )}
-        {name !== "UI/UX Design" &&
-          skills.map((skill) => {
-            const Icon = require(`../../svg/graphsymbols/${slugify(
-              skill.icon
-            )}.svg`).default;
-            return (
-              <button
-                key={skill.icon}
-                className={
-                  activeTechs.length === 0 || activeTechs.includes(skill.icon)
-                    ? "skillIconContainer highLightOnHover"
-                    : "passive skillIconContainer highLightOnHover"
-                }
-                onClick={() => setActiveTech(skill.icon)}
-              >
-                <Icon />
-                <p className={"skillTitle chameleon"}>{skill.icon}</p>
-              </button>
-            );
-          })}
       </div>
       <div className={"projectSidebar"}>
-        {projects.map((project) => (
+        {sortedProjectList.map((project) => (
           <div className={"projectPreviewCardContainer"} key={project.id}>
             <ProjectCard
               project={project}
